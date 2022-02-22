600e737f0d1d054ae99485521ff679f240b9b5b1
Nishant Sahoo <nishants@ideyalabs.com>
2022-02-22 22:33:19 +0530

    added

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -22,58 +22,69 @@ const Search = () => {
     setSearchedItems(filteredArray);
     console.log('filteredArray', filteredArray);
   };
-  useEffect(() => {
 
-   
+  const getTemp = (lat, long, city) => {
+    let temp = {};
+    console.log(lat, long, city);
+    var itemIndex = data.findIndex(str => {
+      return city === str.city;
+      // console.log(str)
+    });
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?lat=' +
+        lat +
+        '&lon=' +
+        long +
+        '&appid=cda02978f791a9630924c13f1c2b0cf7',
+    )
+      .then(response => response.json())
+      .then(json => {
+        temp = json;
+        data[itemIndex].temp = (json.main.temp - 273.15).toFixed(2);
+      });
 
-  }, [searchedItems]);
-
-  const getTemp = (lat, long,city) => {
-console.log(lat,long,city)
-    // fetch(
-    //   'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=cda02978f791a9630924c13f1c2b0cf7',
-    // )
-    //   .then(response => response.json())
-    //   .then(json => console.log(json));
-      var itemIndex = data.findIndex(str => {
-        return city=== str.city     
-        // console.log(str)
-      })
-      console.log(itemIndex)
-      data[itemIndex].temp=38
-      //return str.city===city?(str.temp=38):(str.temp=null) 
- setSearchedItems(data)
- 
+    // console.log(itemIndex, temp);
+    // data[itemIndex].temp = 38;
+    // setSearchedItems(data);
   };
   useEffect(() => {
-
-   
-
-  }, [searchedItems]);
+    setSearchedItems(data);
+  }, [data]);
   return (
     <SafeAreaView>
-      <View style={{ margin: 10 }}>
-
+      <View style={{margin: 10}}>
         <TextInput
           placeholder="Search Cities"
           value={search}
-          style={{ borderColor: '#000', borderWidth: 0.5 }}
+          style={{borderColor: '#000', borderWidth: 0.5}}
           onChangeText={text => searchItem(text)}
         />
         <FlatList
           data={searchedItems}
-          keyExtractor={item => item.id}
-          renderItem={({ item, index }) => {
+          keyExtractor={item => item.city}
+          renderItem={({item, index}) => {
             return (
               <View
-                style={{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', paddingHorizontal: 10, paddingVertical: 10, marginTop: 5 }}
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  backgroundColor: 'white',
+                  paddingHorizontal: 10,
+                  paddingVertical: 10,
+                  marginTop: 5,
+                }}
                 key={index}>
-                <Text style={{ color: 'black', fontSize: 15, }}>{item.city}</Text>
-                <Text>{item.temp?(item.temp):('none')}</Text>
-                <TouchableOpacity onPress={()=>getTemp(item.lat,item.lng,item.city)}
-                style={{ backgroundColor: 'red', padding: 4 }}>
-                  <Text style={{ color: 'white' }}>View Temp</Text>
-                </TouchableOpacity>
+                <Text style={{color: 'black', fontSize: 15}}>{item.city}</Text>
+                {/* <Text>{item.temp ? item.temp : 'none'}</Text> */}
+                {!item.temp ? (
+                  <TouchableOpacity
+                    onPress={() => getTemp(item.lat, item.lng, item.city)}
+                    style={{backgroundColor: 'red', padding: 4}}>
+                    <Text style={{color: 'white'}}>View Temp</Text>
+                  </TouchableOpacity>
+                ) : (
+                  <Text>{item.temp}</Text>
+                )}
               </View>
             );
           }}
