15abf0748f90d8825ec9be172efcc9280c807b38
Harjasilin Pradhan <harjasilinp@ideyalabs.com>
2022-02-22 13:38:00 +0530

    harx

--- src/page.js
+++ src/page.js
@@ -10,12 +10,22 @@ import {
   View,
 } from 'react-native';
 import data from './Indiancities.json';
-
 const page = () => {
   console.log('data', data);
-  return <View></View>;
+  return (
+  <View style={{width:'100%',backgroundColor:'white',paddingHorizontal:10,paddingVertical:10,marginTop:10,flexDirection:'row',justifyContent:'space-between'}}>
+<Text>
+  Angul:
+</Text>
+<Text>
+  32.23
+</Text>
+  </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+});
 
 export default page;
